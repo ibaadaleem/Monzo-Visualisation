97f1c80a280a1df18264a6b5f14b0d9ef63fb9dd
Ibaad Aleem <ibaad.aleem@gmail.com>
2017-11-23 23:13:51 +0000

    add tests

--- visualisation/tests.py
+++ visualisation/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import MonzoCsvExport
+
+class MonzoHomeView(TestCase):
+	def test_model_is_empty(self):
+		'''
+		If no records exist in the model then make sure screen doesn't error
+		'''
+		response = self.client.get(reverse('visualisation:home'))
+		self.assertEqual(response.status_code,200)
+		
+class MonzoVisualsView(TestCase):
+	def test_model_is_empty(self):
+		'''
+		If no records exist in the model then make sure screen doesn't error
+		'''
+		response = self.client.get(reverse('visualisation:visuals'))
+		self.assertEqual(response.status_code,200)
+
+class MonzoAnalyticsView(TestCase):
+	def test_model_is_empty(self):
+		'''
+		If no records exist in the model then make sure screen doesn't error
+		'''
+		response = self.client.get(reverse('visualisation:analytics'))
+		self.assertEqual(response.status_code,200)
+		
+class MonzoUploadView(TestCase):
+	def test_model_is_empty(self):
+		'''
+		If no records exist in the model then make sure screen doesn't error
+		'''
+		response = self.client.get(reverse('visualisation:upload'))
+		self.assertEqual(response.status_code,200)
+		
+class MonzoCsvView(TestCase):
+	def test_model_is_empty(self):
+		'''
+		If no records exist in the model then make sure screen doesn't error
+		'''
+		response = self.client.get(reverse('visualisation:csv_data'))
+		self.assertEqual(response.status_code,200)
